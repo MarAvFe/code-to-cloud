2560f5902844006a13f7bdf4fce18139395d0377
marcello <marcelloavila@gmail.com>
2020-08-17 13:00:41 -0600

    fix: tf local reference name

--- tf/eks-cluster.tf
+++ tf/eks-cluster.tf
@@ -1,3 +1,10 @@
+locals {
+    asg_desired_capacity = 1
+    asg_max_size = 3
+    asg_min_size = 1
+    instance_type = "t2.small"
+}
+
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
   cluster_name = local.cluster_name
@@ -14,14 +21,14 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = var.instance_type
-      asg_desired_capacity          = var.asg_desired_capacity
+      instance_type                 = local.instance_type
+      asg_desired_capacity          = local.asg_desired_capacity
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = var.instance_type
-      asg_desired_capacity          = var.asg_desired_capacity
+      instance_type                 = local.instance_type
+      asg_desired_capacity          = local.asg_desired_capacity
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
     },
   ]
@@ -33,11 +40,4 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
-}
-
-locals {
-    asg_desired_capacity = 1
-    asg_max_size = 3
-    asg_min_size = 1
-    instance_type = "t2.small"
 }
